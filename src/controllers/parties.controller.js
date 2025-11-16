4931fccde1a616e9003f61e9df616deee63914dd
JavierAQR <nonamesteam123@gmail.com>
2025-11-15 20:36:25 -0500

    fix: corregidos errores de importacion de prisma en controladores

--- src/controllers/parties.controller.js
+++ src/controllers/parties.controller.js
@@ -1,3 +1,5 @@
+import prisma from "../lib/prisma.js";
+
 export const getParties = async (req, res) => {
   try {
     const parties = await prisma.party.findMany({
--- src/controllers/voters.controller.js
+++ src/controllers/voters.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma";
+import prisma from "../lib/prisma.js";
 
 export const getVoterByDni = async (req, res) => {
     const { dni } = req.params;
