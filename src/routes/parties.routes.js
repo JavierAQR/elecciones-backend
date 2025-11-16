f652a5bc1c290d23f574c3a35739d5cb91f9a415
JavierAQR <nonamesteam123@gmail.com>
2025-11-15 20:38:28 -0500

    fix: corregido app a router a parties.routes

--- src/routes/parties.routes.js
+++ src/routes/parties.routes.js
@@ -3,7 +3,8 @@ import * as partiesController from "../controllers/parties.controller.js";
 
 const router = Router()
 
-app.get("/parties", partiesController.getParties);
+router.get("/parties", partiesController.getParties);
+
 router.get("/:id", partiesController.getPartyById);
 
 
