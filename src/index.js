71e340fd033b14494243c991c4d5cf20b4676fab
JavierAQR <nonamesteam123@gmail.com>
2025-11-16 11:57:16 -0500

    feat: Agregados endpoints para voters

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import eventRoutes from "./routes/event.routes.js";
 import partiesRoutes from "./routes/parties.routes.js"
 import guidesRoutes from "./routes/guides.routes.js"
+import votersRoutes from "./routes/voters.routes.js"
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use("/api/events", eventRoutes)
 app.use("/api/parties", partiesRoutes)
 app.use("/api/guides", guidesRoutes)
+app.use("/api/voters", votersRoutes)
 
 export default app;
